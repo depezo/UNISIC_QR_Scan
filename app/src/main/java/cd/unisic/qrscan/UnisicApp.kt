2fadbf841b5815ba3b8abb8c272a6cb662d0a612
Depezo <depezoawacd@gmail.com>
2026-04-02 16:10:27 +0100

    feat: initialize Hilt application class for dependency injection

--- app/src/main/java/cd/unisic/qrscan/UnisicApp.kt
+++ app/src/main/java/cd/unisic/qrscan/UnisicApp.kt
@@ -4,4 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
+//Trouvez le carburant le moins cher autour de vous et suivez facilement vos dépenses.
 class UnisicApp : Application()
